e1534d49e2c2730e2388b57f209b8a3b9fc09b76
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-05-28 20:00:00 +0300

    feat: update splash screen and app layout with new logo and background color

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,43 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import { useColorScheme } from 'react-native';
+import AppTabs from "@/components/app-tabs";
+import { DarkTheme, DefaultTheme, ThemeProvider } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
+import { useColorScheme } from "react-native";
+import Splash from "./splash";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+SplashScreen.preventAutoHideAsync();
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
+  const [splashDone, setSplashDone] = useState(false);
+
+  useEffect(() => {
+    // var for the timer
+    let timer;
+    // prepare the splash screen
+    async function prepare() {
+      try {
+        const timer = setTimeout(() => {
+          setSplashDone(true);
+        }, 3000);
+        await SplashScreen.hideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+    }
+    // clear the timer in the component life cycle
+    () => {
+      clearTimeout(timer);
+    };
+    prepare();
+  }, []);
+
+  if (!splashDone) {
+    return <Splash />;
+  }
+
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <AppTabs />
     </ThemeProvider>
   );
